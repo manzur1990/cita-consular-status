c8d77ef9d0bcc363e4dbd3d35c6714fa4d70b6ea
manzur1990 <manzur-1990@hotmail.com>
2023-05-19 14:34:10 -0600

    work in process

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
       display: flex;
       justify-content: center;
       align-items: center;
-      height: 100vh;
       height: 40vh;
     }
 
@@ -55,7 +54,7 @@
         <form method="post" onsubmit="return validateForm()">
           <div class="form-group">
             <label for="numbersInput"></label>
-            <input type="text" class="form-control" id="identidad" name="identidad" placeholder="Número de Identidad" onkeydown="limitInput(event)">
+            <input type="text" class="form-control" id="identidad" name="identidad" placeholder="Número de Identidad">
           </div>
           <button type="submit" class="btn btn-primary">Buscar</button>
         </form>
@@ -117,6 +116,9 @@
       if ($results) {
         // Display the user information in a table
         echo '<div class="container">
+                <div class="print_button_container">
+                 <button type="button" class="btn btn-secondary" onclick="printTable()"><i class="fas fa-print"></i></button>
+                </div>
                         <table class="table table-striped">
                             <tbody>';
 
@@ -184,53 +186,82 @@
 
   <!-- Bootstrap JS -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <script>
-    function validateForm(event) {
-      // Prevent form submission
-      event.preventDefault();
+    function validateForm() {
+      var identidadInput = document.getElementById('identidad');
+      var identidadValue = identidadInput.value.trim();
 
-      // Validation code...
-      var inputField = document.getElementById('identidad');
-      var identidadValue = inputField.value;
+      // Remove non-numeric characters
+      identidadValue = identidadValue.replace(/\D/g, '');
+      identidadInput.value = identidadValue;
 
-      if (!/^\d{0,12}$/.test(identidadValue)) {
-        alert('La identidad debe ser un número de hasta 12 dígitos.');
+      // Validate length
+      if (identidadValue.length !== 13) {
+        identidadInput.classList.add('is-invalid');
         return false;
       }
+
+      identidadInput.classList.remove('is-invalid');
+      return true;
     }
 
-    // Add keydown event listener to limit the input length
-    function limitInput(event) {
-      var inputField = document.getElementById('identidad');
-      var identidadValue = inputField.value;
+    // Allow only numeric input (including 10-key number pad) and limit to 12 characters
+    document.getElementById('identidad').addEventListener('keydown', function(event) {
+      var allowedKeys = [8, 9, 37, 39, 46]; // Backspace, Tab, Left Arrow, Right Arrow, Delete
 
-      if (identidadValue.length >= 12 && event.keyCode !== 8 && event.keyCode !== 46) {
+      // Allow numeric keys from 10-key number pad
+      var numberPadKeys = [96, 97, 98, 99, 100, 101, 102, 103, 104, 105];
+      allowedKeys = allowedKeys.concat(numberPadKeys);
+
+      if (event.altKey || event.metaKey) {
+        return; // Allow keyboard shortcuts
+      }
+
+      if (allowedKeys.indexOf(event.keyCode) !== -1) {
+        return; // Allow navigation and deletion keys
+      }
+
+      if (event.keyCode < 48 || event.keyCode > 57) {
+        event.preventDefault(); // Prevent input of non-numeric characters
+      }
+
+      var identidadInput = document.getElementById('identidad');
+      var identidadValue = identidadInput.value.trim();
+
+      // Limit to 13 characters
+      if (identidadValue.length >= 13) {
         event.preventDefault();
       }
+    });
+
+    // Prevent pasting non-numeric characters
+    document.getElementById('identidad').addEventListener('input', function(event) {
+      var identidadInput = event.target;
+      var identidadValue = identidadInput.value.trim();
+      identidadValue = identidadValue.replace(/\D/g, '');
+      identidadInput.value = identidadValue;
+
+      // Limit to 13 characters
+      if (identidadValue.length > 13) {
+        identidadInput.value = identidadValue.slice(0, 13);
+      }
+    });
+  </script>
+
+  <script>
+    function printTable() {
+      document.querySelector('.navbar').style.display = 'none';
+      document.querySelector('.col-md-6').style.display = 'none';
+
+      window.print();
+
+      document.querySelector('.navbar').style.display = 'block';
+      document.querySelector('.col-md-6').style.display = 'block';
     }
-
-    // Enable Alt + C for copy functionality
-    window.addEventListener('keydown', function(event) {
-      if (event.altKey && event.code === 'KeyC') {
-        event.preventDefault();
-      }
-    });
-
-    // Enable Alt + A functionality
-    window.addEventListener('keydown', function(event) {
-      if (event.altKey && event.code === 'KeyA') {
-        event.preventDefault();
-      }
-    });
-
-    // Enable Ctrl + V functionality (paste command)
-    window.addEventListener('keydown', function(event) {
-      if (event.ctrlKey && event.code === 'KeyV') {
-        event.preventDefault();
-      }
-    });
   </script>
 </body>
 
 </html>
+
+
+<!-- 1701198801689 -->
